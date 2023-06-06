f6549e777eff2e395d2fcfc15a2db9152e8f10ce
BhaktiRaval <bhaktiraval112001@gmail.com>
2023-06-06 18:41:40 +0530

    created api for User login and register

--- MedicaBE/MedicaBE/Controllers/UserController.cs
+++ MedicaBE/MedicaBE/Controllers/UserController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MedicaBE.Entities.ViewModels;
+using MedicaBE.Repository.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MedicaBE.Controllers
-{
+namespace MedicaBE.Controllers;
+
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository _iuserRepository)
+        {
+            userRepository = _iuserRepository;
+        }
+
+        [HttpPost("User login")]
+        public IActionResult UserLogin(UserLoginViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = userRepository.ValidateUser(user);
+                return Ok(check);
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return BadRequest(errors);
+            }
+
+        }
+
+        [HttpPost("Register user")]
+        public IActionResult RegisterUser(UserRegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = userRepository.RegisterUser(user);
+                return Ok(result);
+            }
+            else
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return BadRequest(errors);
+            }
+
+        }
 
     }
-}
+
--- MedicaBE/MedicaBE/Program.cs
+++ MedicaBE/MedicaBE/Program.cs
@@ -1,19 +1,24 @@
 using MedicaBE.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using MedicaBE.Repository;
+using MedicaBE.Repository.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEncryptDecryptPassword, EncryptDecryptPassword>();
+
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
 builder.Services.AddSingleton<DatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString")));
 
 // Add services to the container.
-//mongodb+srv://bhaktiravaltatvasoft:<password>@cluster0.huxpk70.mongodb.net/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -32,3 +37,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
